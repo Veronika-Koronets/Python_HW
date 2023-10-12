565d185103fffd49f43860ef4970e7b65bf209d7
Veronika <130933875+Veronika-Koronets@users.noreply.github.com>
2023-10-12 17:00:42 +0300

    Update HW Collections P1.py

--- HW Collections P1.py
+++ HW Collections P1.py
@@ -14,6 +14,7 @@ for i in range(1,51):
 
 
 # 2. Преобразовать список Emails в Dict где ключ - это index элемента в списке
+
 # 3. Создать список Name_Surname из 100 элементов, где каждое 10 имя “Adam”
 # [“Adam_black”, “Olga_Savitskaya, …]
 # 4. Создать список списков в котором будут пары
@@ -24,4 +25,7 @@ for i in range(1,51):
 #
 # 5. Сгенерировать список из 100 кличек для животных , в списке должно быть 10 одинаковых кличек
 # [“Spike”, “Barboss”, “Bloom”, “Spike”, …]
+https://mooc.nspu.ru/pluginfile.php/33768/mod_resource/content/0/%D0%9F%D1%80%D0%B8%D0%BC%D0%B5%D1%80%D1%8B%20%D1%80%D0%B5%D1%88%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%B7%D0%B0%D0%B4%D0%B0%D1%87.pdf
 # 6. Найти дубликаты в списке кличек и удалить их из списка, оставив один экземпляр клички дубликатов.
+Это можно сделать путём преобразования списка во множество (set, иногда ещё его называют «набор»), а затем обратно в список:
+https://skillbox.ru/media/code/spiski_v_python_11_voprosov_kotorye_mogut_zadat_na_sobesedovanii/
